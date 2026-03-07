33bcbe40220c5536a33071708571c22e7157a122
Mariano <marianolarrete@gmail.com>
2026-03-07 13:37:58 -0300

    Updated functions

--- placid-hydra/hydra-voronoifix.js
+++ placid-hydra/hydra-voronoifix.js
@@ -1,32 +1,4 @@
-{
-    const getHydra = function () {
-        const whereami = window.location?.href?.includes("hydra.ojack.xyz")
-            ? "editor"
-            : window.atom?.packages
-            ? "atom"
-            : "idk";
-        if (whereami === "editor") {
-            return window.hydraSynth;
-        }
-        if (whereami === "atom") {
-            return global.atom.packages.loadedPackages["atom-hydra"]
-                .mainModule.main.hydra;
-        }
-        let _h = [
-            window.hydraSynth,
-            window._hydra,
-            window.hydra,
-            window.h,
-            window.H,
-            window.hy
-        ].find(h => h?.regl);
-        return _h;
-    };
-    window._hydra = getHydra();
-    window._hydraScope = _hydra.sandbox.makeGlobal ? window : _hydra.synth;
-}
-
-_hydra.setFunction({
+setFunction({
   name: "voronoifix",
   type: "src",
   inputs: [
