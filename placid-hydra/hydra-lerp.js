8da8da234072dcd9e9d4806431b5b6af288f93b9
Mariano <marianolarrete@gmail.com>
2026-03-21 14:18:36 -0300

    I messed it up myself

--- placid-hydra/hydra-lerp.js
+++ placid-hydra/hydra-lerp.js
@@ -2,8 +2,8 @@ setFunction({
   name: "lerp",
   type: "combine",
   inputs: [
-    { name: "lowTex", type: "sampler2D" },
-    { name: "highTex", type: "sampler2D" }
+    { name: "a", type: "sampler2D" },
+    { name: "b", type: "sampler2D" }
   ],
   glsl: `
   return a * (1.0 - _c0) + b * _c0;
