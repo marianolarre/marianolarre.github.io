4f06bb5a0e29a28415560ba6f71204444f704417
Mariano <marianolarrete@gmail.com>
2026-03-21 14:18:19 -0300

    Fine, ill do it myself

--- placid-hydra/hydra-lerp.js
+++ placid-hydra/hydra-lerp.js
@@ -6,11 +6,6 @@ setFunction({
     { name: "highTex", type: "sampler2D" }
   ],
   glsl: `
-  float t = dot(_c0.rgb, vec3(0.299, 0.587, 0.114));
-
-  vec4 low = texture2D(lowTex, _st);
-  vec4 high = texture2D(highTex, _st);
-
-  return mix(low, high, t);
+  return a * (1.0 - _c0) + b * _c0;
   `
 })
