4acac0f84073719b2cf7abc7a967381f9688eaf4
Mariano <marianolarrete@gmail.com>
2026-03-22 01:47:23 -0300

    Update hydra-colorgradient.js

--- placid-hydra/hydra-colorgradient.js
+++ placid-hydra/hydra-colorgradient.js
@@ -12,6 +12,7 @@ setFunction({
   glsl: `
   vec4 c = _c0;
   float t = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+  t = clamp(t, 0.0, 1.0);
   vec3 colA = vec3(rA, gA, bA);
   vec3 colB = vec3(rB, gB, bB);
   vec3 result = mix(colA, colB, t);
